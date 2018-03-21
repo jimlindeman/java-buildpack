35bf7185f25c264a3f541e69160db232711f85ae
Jim Lindeman <lindj@us.ibm.com>
2018-03-21 03:28:58 +0000

    More fixes

--- lib/java_buildpack/buildpack.rb
+++ lib/java_buildpack/buildpack.rb
@@ -83,7 +83,7 @@ module JavaBuildpack
       @logger.info( "Droplet.root = #{droplet.root}" )
       @logger.info( "LSF-file dir is #{lsf_dir}" )
       FileUtils.mkdir_p lsf_target_dir
-      FileUtils.cp_r(lsf_dir + '/*',  lsf_target_dir)
+      FileUtils.cp_r(lsf_dir, lsf_target_dir)
 
       log_cache_contents
     end
